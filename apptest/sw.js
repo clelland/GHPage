04751e8cffaebffedcfa336473cb49a34923467c
Ian Clelland <iclelland@chromium.org>
2014-12-05 23:34:00 -0500

    Oops extra semicolon

--- apptest/sw.js
+++ apptest/sw.js
@@ -3,6 +3,6 @@ self.addEventListener('install', function(ev) {
     caches.open('packaged-assets-1.0.0').then(function(cache) {
       console.log("Cache opened!");
       return cache.add('/index.html');
-    });
+    })
   );
 });
