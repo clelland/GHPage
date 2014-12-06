71329031bd575f47bd1ad1155e343e14b75354fd
Ian Clelland <iclelland@chromium.org>
2014-12-06 00:00:55 -0500

    Switch to actually-supported cache API

--- apptest/sw.js
+++ apptest/sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(ev) {
   ev.waitUntil(
     caches.open('packaged-assets-1.0.0').then(function(cache) {
       console.log("Cache opened!");
-      var r = cache.add('/index.html');
+      var r = cache.put('/index.html');
       return r;
     })
   );
